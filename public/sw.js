5d84d20f20e614dad13e58682ce82bc38e37f653
ThegreatHamza <hamzamohamedmoud@gmail.com>
2026-09-22 17:19:34 +0300

    Add offline service worker

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='civil-tools-v1';
+const ASSETS=['/','/index.html','/manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res}).catch(()=>caches.match('/')))});
