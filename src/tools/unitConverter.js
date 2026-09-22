541ef3dc439dec8a89580dcea4f3b1301c1dfc23
ThegreatHamza <hamzamohamedmoud@gmail.com>
2026-09-22 16:54:22 +0300

    Add basic engineering unit converter

---
+++ src/tools/unitConverter.js
@@ -0,0 +1,6 @@
+export const conversions = {
+  mmToM: (value) => Number(value) / 1000,
+  mToMm: (value) => Number(value) * 1000,
+  kgToTon: (value) => Number(value) / 1000,
+  tonToKg: (value) => Number(value) * 1000
+};
