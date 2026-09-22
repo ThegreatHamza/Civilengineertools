7836b82733a8b318c840168a66a7bd6b21d12558
ThegreatHamza <hamzamohamedmoud@gmail.com>
2026-09-22 17:20:24 +0300

    Make service worker registration work on hosted subpaths

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './app.css';
 
-if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(()=>{}));
+if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js').catch(()=>{}));
 createRoot(document.getElementById('root')).render(<App />);
