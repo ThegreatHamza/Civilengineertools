3c301f4506e1691bbbe6c141d04bf968b33d5389
ThegreatHamza <hamzamohamedmoud@gmail.com>
2026-09-22 17:19:20 +0300

    Use complete app stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './style.css';
+import './app.css';
 
+if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(()=>{}));
 createRoot(document.getElementById('root')).render(<App />);
